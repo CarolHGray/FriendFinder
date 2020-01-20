7ac0b05a8d72659c81001beb4fd4dbab9914ca14
CarolHGray <56186028+CarolHGray@users.noreply.github.com>
2020-01-20 02:08:08 -0500

    Add files via upload for Friend Finder application

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const fs = require("fs");
+const fileName = "../data/friends.json";
+const file = require(fileName);
+
+router.get("/api/friends", (req, res) => {
+  res.json(file);
+});
+
+router.post("/api/friends", (req, res) => {
+  const Scores = req.body.scores.map(score => parseInt(score));
+
+  let matchNumber = null;
+  let matches = [];
+
+  for (let friend of file) {
+    let difference = 0;
+    friend.scores.map((score, idx) => {
+      difference += Math.abs(score - Scores[idx]);
+    });
+
+    if (matchNumber === null || matchNumber > difference) {
+      matchNumber = difference;
+      matches.length = 0;
+      matches.push(friend);
+    } else if (matchNumber === difference) {
+      matches.push(friend);
+    }
+  }
+  const NewFriend = req.body;
+  NewFriend.scores = Scores;
+  const UpdatedFile = [...file, NewFriend];
+
+  fs.writeFile(
+    path.join(__dirname, "../data/friends.json"),
+    JSON.stringify(UpdatedFile),
+    err => {
+      if (err) throw err;
+      console.log("it worked!");
+      res.json(matches);
+    }
+  );
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("app/img"));
+
+//handles parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var apiRoutes = require("./app/routing/apiRoutes");
+app.use(apiRoutes);
+var htmlRoutes = require("./app/routing/htmlRoutes");
+app.use(htmlRoutes);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
